44d54a5cabcb51a19bad06f939af6364bfd80bdc
aleksandraml <aleksandramentel1525@gmail.com>
2023-01-18 10:26:16 +0100

    poprawka

--- 40-js-dane-formularz/main.js
+++ 40-js-dane-formularz/main.js
@@ -1,6 +1,19 @@
-let download = document.getElementById("surname");
-function result(surname) {
-    const elem = document.getElementById('surname');
+function result() {
+    const inputSurname = document.getElementById('surname').value;
+    const nazwisko = document.querySelector('.surname');
+    const inputName = document.getElementById('name').value;
+    const imię = document.querySelector('.name');
+    const inputMessage = document.getElementById('message').value;
+    const treść = document.querySelector('.message');
+
+    console.log(treść)
+
+    nazwisko.innerText = inputSurname
+    imię.innerText = inputName
+    treść.innerText = inputMessage
 }
 
-console.log(surname)
+
+let button = document.querySelector('button')
+
+button.addEventListener('click', result)
