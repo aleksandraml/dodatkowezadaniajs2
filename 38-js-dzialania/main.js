c5f7b7a976c95cdc918ba057d49a853046d31cc5
aleksandraml <aleksandramentel1525@gmail.com>
2023-01-20 11:32:26 +0100

    poprawka

--- 38-js-dzialania/main.js
+++ 38-js-dzialania/main.js
@@ -1,10 +1,25 @@
-function math(x, y )
-let sum = x + y;
-let difference = x - y;
-let multiplication = x * y;
-if (result < 0){
-    console.log('Podany wynik jest nieprawidłowy')
+function math(x, y) {
+  let sum = x + y;
+ if (sum > 0) {
+    console.log('Wynik dodawania wynosi ' + sum);
+  } else {
+    console.log('Wynik jest nieprawidłowy!');
+  }
+  let multiply = x * y;
+  if (multiply > 0) {
+    console.log('Wynik mnożenia wynosi ' + multiply);
+  } else {
+    console.log('Wynik jest nieprawidłowy!');
+  }
+
+  let subtraction = x - y;
+  if (subtraction > 0) {
+    console.log('Wynik odejmowania wynosi ' + subtraction);
+  } else {
+    console.log('Wynik jest nieprawidłowy!');
+  }
+
 }
 
 
-
+console.log(math(4, 5))
