24d0d9aeb7f44cd47e5dba414099862bab9bd132
aleksandraml <aleksandramentel1525@gmail.com>
2023-01-20 11:33:45 +0100

    poprawka

--- 37-js-dodaj-usun-tekst/main.js
+++ 37-js-dodaj-usun-tekst/main.js
@@ -1,6 +1,24 @@
-let mybutton = document.querySelector('tekst');
+let first = document.querySelector('#tekst');
+console.log(first);
 
-mybutton.textContent = 'Przykładowy tekst';
+function show() {
+   document.getElementById('tekst').innerText = 'Przykładowy tekst';
 
-let yourbutton = document.querySelector('buttonsecond');
-yourbutton.textContent = deletes ;
+}
+
+let second = document.querySelector('#tekst');
+
+function clean() {
+   second.innerText = '';
+
+}
+
+let buttonShow = document.querySelector('.first')
+let buttonClean = document.querySelector('.second')
+
+buttonShow.addEventListener('click', show)
+buttonClean.addEventListener('click', clean)
+
+
+
+
